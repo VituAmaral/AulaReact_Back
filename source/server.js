5ef35dd07db680472dc059399e8f744cbed6bd59
victorflorindo9@gmail.com <19180130@CFP127ALUNO.LOCAL>
2020-02-13 16:26:07 -0300

    Aplicando bootstrap

--- source/server.js
+++ source/server.js
@@ -6,6 +6,7 @@ app.use(express.json())
 
 let alowCrossDomain = (req, res, next) =>{
     res.header('Access-Control-Allow-Origin', "*")
+    res.header('Access-Control-Allow-Methods', "*")
     res.header('Access-Control-Allow-Headers', "*")
     next()
 }
